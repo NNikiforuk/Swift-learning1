07c988080030f69069bab3ea1866fc62d3ccc3f0
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-08-21 16:18:55 +0200

    Simple list

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Natalia Nikiforuk on 21/08/2024.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
